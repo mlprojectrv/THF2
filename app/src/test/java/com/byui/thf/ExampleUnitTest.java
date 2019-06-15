29dac101c0c3b9beed67bb5b3439e2b9d97c4566
AkivaKT <tun18001@byui.edu>
2019-06-14 19:08:07 -0600

    updated encryption class

--- app/src/test/java/com/byui/thf/ExampleUnitTest.java
+++ app/src/test/java/com/byui/thf/ExampleUnitTest.java
@@ -10,10 +10,6 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void data_Input_valid() {

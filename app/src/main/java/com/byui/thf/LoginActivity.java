24c61ba2eba542cee54316c0bc6d939a1f36c960
hyeonbeom lee <hyeonbeom920723@gmail.com>
2019-06-17 17:46:38 -0600

    Implement Activities again (UI)

--- app/src/main/java/com/byui/thf/LoginActivity.java
+++ app/src/main/java/com/byui/thf/LoginActivity.java
@@ -11,6 +11,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        setTitle("HansomeTieFactory");
+
         Intent intent = getIntent();
 
 
--- app/src/main/java/com/byui/thf/MainActivity.java
+++ app/src/main/java/com/byui/thf/MainActivity.java
@@ -4,25 +4,65 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText id;
+    private EditText password;
+    private Button login;
+    private TextView info;
+    private int loginCounter = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         setTitle("HansomeTieFactory");
+
+        id = (EditText)findViewById(R.id.typeId);
+        password = (EditText)findViewById(R.id.typePassword);
+        login = (Button)findViewById(R.id.clickLogin);
+        info = (TextView)findViewById(R.id.incorrect);
+
+        info.setText("Remained attemps: 5");
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(id.getText().toString(), password.getText().toString());
+            }
+        });
+
     }
 
-    public void checkLogin(View theButton) {
+    private void validate(String userName, String userPassword) {
         // check id and password in this function.
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+        else {
+            loginCounter--;
+
+            info.setText("Remained attemps: " + String.valueOf(loginCounter));
+
+            if (loginCounter == 0) {
+                login.setEnabled(false);
+            }
+
+        }
     }
 
     public void successLogin(View theButton) {
-        Intent loginIntent = new Intent(this, LoginActivity.class);
-        startActivity(loginIntent);
+
     }
 
     public class Sales {

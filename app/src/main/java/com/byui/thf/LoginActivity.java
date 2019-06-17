9eabb6b0a0f1430700411097e2a790d55eff1772
hyeonbeom lee <hyeonbeom920723@gmail.com>
2019-06-17 02:14:27 -0600

    Implement Activities(UI)

---
+++ app/src/main/java/com/byui/thf/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.byui.thf;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
+
+
+    }
+}

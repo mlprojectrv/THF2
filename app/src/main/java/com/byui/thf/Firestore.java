08295e119db0630fb3797d7dbf4dc8b05d758596
Your Name <sy17001@byui.edu>
2019-06-22 19:52:20 -0600

    Merge branch 'master' of https://github.com/AkivaKT/THF2
    # Conflicts:
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/byui/thf/Firestore.java
@@ -0,0 +1,46 @@
+package com.byui.thf;
+
+public class Firestore {
+
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Add Data
+    // Create a new user with a first and last name
+    Map<String, Object> user = new HashMap<>();
+    user.put("first", "Ada");
+    user.put("last", "Lovelace");
+    user.put("born", 1815);
+
+    // Add a new document with a generated ID
+    db.collection("users")
+        .add(user)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+
+    // Read Data
+    db.collection("users")
+            .get()
+        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+                }
+            } else {
+                Log.w(TAG, "Error getting documents.", task.getException());
+            }
+        }
+    });
+}

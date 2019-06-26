1e135f41d06f2a577978e3b5b6e863c18270e957
Your Name <sy17001@byui.edu>
2019-06-26 16:18:34 -0600

    Firestore Updates

--- app/src/main/java/com/byui/thf/Firestore.java
+++ app/src/main/java/com/byui/thf/Firestore.java
@@ -4,19 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.
-import java.util.Collection;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import static android.content.ContentValues.TAG;
 
 public class Firestore {
 
+    private final FirebaseFirestore db;
     private DatabaseReference mDatabase;
+    private String Product;
+    private String Sales;
+    private String Price;
 
-    mDatabase = FirebaseDatabase.getInstance().getReference();
+    public Firestore(FirebaseFirestore db) {
+        this.db = db;
+    }
 
+    public void setup() {
+        // [START get_firestore_instance]
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // [END get_firestore_instance]
+
+        // [START set_firestore_settings]
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+        // [END set_firestore_settings]
+    }
+    public void StoreJson(){
+
+        Map<String, Object> Update = new HashMap<>();
+        Update.put("Product", Product);
+        Update.put("Sales", Sales);
+        Update.put("Price", Price);
+
+        db.collection("JsonUpdate")
+                .add(Update)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
 
     /*
     // Access a Cloud Firestore instance from your Activity

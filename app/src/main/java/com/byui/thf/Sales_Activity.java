936a18c73d049352095b26e64c80920b5d78b6c5
Woojung <son15005@byui.edu>
2019-06-28 10:08:09 -0600

    woo commits. sales_activity.

---
+++ app/src/main/java/com/byui/thf/Sales_Activity.java
@@ -0,0 +1,40 @@
+package com.byui.thf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static android.R.layout.simple_spinner_item;
+
+public class Sales_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_price_);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(context: this, R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}

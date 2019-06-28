7bcfc1bf274bf9a42968e700eece62cdbaa59124
Your Name <sy17001@byui.edu>
2019-06-28 17:41:36 -0600

    Firestore Updates

--- app/src/main/java/com/byui/thf/Firestore.java
+++ app/src/main/java/com/byui/thf/Firestore.java
@@ -1,5 +1,15 @@
 package com.byui.thf;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,66 +18,49 @@ import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.gson.Gson;
 
 import static android.content.ContentValues.TAG;
 
 public class Firestore {
 
-    private final FirebaseFirestore db;
-    private DatabaseReference mDatabase;
+    //private final FirebaseFirestore db;
+   // private DatabaseReference mDatabase;
     private String Product;
     private String Sales;
     private String Price;
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("JsonUpdate/Q4PGpcjnBEAE6CiQSbD5");
 
-    public Firestore(FirebaseFirestore db) {
+   /*  public Firestore(FirebaseFirestore db) {
         this.db = db;
     }
 
     public void setup() {
-        // [START get_firestore_instance]
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // [END get_firestore_instance]
 
-        // [START set_firestore_settings]
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
                 .setPersistenceEnabled(true)
                 .build();
         db.setFirestoreSettings(settings);
-        // [END set_firestore_settings]
-    }
-    public void StoreJson(){
+
+    } */
+    public void StoreJson(String Product, String Sales, String Price){
 
         Map<String, Object> Update = new HashMap<>();
         Update.put("Product", Product);
         Update.put("Sales", Sales);
         Update.put("Price", Price);
 
-        db.collection("JsonUpdate")
-                .add(Update)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        mDocRef.set(Update).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "DocumentSnapshot written with ID");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Json was not saved!", e);
+            }
+        });
 
     }
 
@@ -85,49 +78,5 @@ public class Firestore {
         }
         return file;
     }
-
-    /*
-    // Access a Cloud Firestore instance from your Activity
-    // FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    // Add Data
-    // Create a new user with a first and last name
-    // Map<String, Object> user = new HashMap<>();
-    // user.put("first", "Ada");
-    ''' user.put("last", "Lovelace");
-    user.put("born", 1815);
-
-    // Add a new document with a generated ID
-    db.collection("users")
-        .add(user)
-        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-            }
-        })
-        .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w(TAG, "Error adding document", e);
-            }
-        });
-
-    // Read Data
-    db.collection("users")
-            .get()
-        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-        @Override
-        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Log.d(TAG, document.getId() + " => " + document.getData());
-                }
-            } else {
-                Log.w(TAG, "Error getting documents.", task.getException());
-            }
-        }
-    });
-    */
 
 }

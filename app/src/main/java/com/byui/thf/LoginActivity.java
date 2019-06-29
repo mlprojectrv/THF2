c3c6ee616a97cf35575ec245f1dc3033ace80403
AkivaKT <tun18001@byui.edu>
2019-06-28 19:08:32 -0600

    activity

--- app/src/main/java/com/byui/thf/LoginActivity.java
+++ app/src/main/java/com/byui/thf/LoginActivity.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import static android.R.layout.simple_spinner_item;
+
+public class LoginActivity  extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button prob;
     private Button prib;
@@ -27,12 +33,37 @@ public class LoginActivity extends AppCompatActivity {
         prib = (Button)findViewById(R.id.price);
         saleb =(Button)findViewById(R.id.sale);
 
+        Spinner spinner = findViewById(R.id.spinner1);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers, simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
 
         saleb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent saleIntent = new Intent(LoginActivity.this, Sales_Activity.class);
-                startActivity(saleIntent);
+                salesPage();
             }});
+    }
+
+    private void salesPage(){
+        Intent saleIntent = new Intent(LoginActivity.this, Sales_Activity.class);
+        startActivity(saleIntent);
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }

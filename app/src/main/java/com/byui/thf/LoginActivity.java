0859f3f1d9adc0fe8bf1e2b28d6939b6e64621b2
AkivaKT <tun18001@byui.edu>
2019-06-28 18:02:56 -0600

    updated sale_Activity

--- app/src/main/java/com/byui/thf/LoginActivity.java
+++ app/src/main/java/com/byui/thf/LoginActivity.java
@@ -3,8 +3,15 @@ package com.byui.thf;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button prob;
+    private Button prib;
+    private Button saleb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +19,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         // Title
         setTitle("HansomeTieFactory");
+        // data received from main
         Intent intent = getIntent();
 
+        // 3 button and listeners
+        prob = (Button)findViewById(R.id.product);
+        prib = (Button)findViewById(R.id.price);
+        saleb =(Button)findViewById(R.id.sale);
 
+
+        saleb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent saleIntent = new Intent(LoginActivity.this, Sales_Activity.class);
+                startActivity(saleIntent);
+            }});
     }
 }
